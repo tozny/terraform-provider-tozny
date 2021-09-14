d9803f6be2bd6a29a1e38c6e88244b11b4ce48fc
Levi Sky <levi@tozny.com>
2021-09-14 13:47:10 -0700

    Version bumps for 0.14.0 release.

--- Makefile
+++ Makefile
@@ -1,5 +1,5 @@
 BINARY=terraform-provider-tozny
-VERSION=0.13.0
+VERSION=0.14.0
 
 default: build
 
--- examples/realms/applications/SAMLDescription/main.tf
+++ examples/realms/applications/SAMLDescription/main.tf
@@ -8,7 +8,7 @@ terraform {
       # for Tozny registry.terraform.io/tozny
       source  = "tozny/tozny"
       # Pin Tozny provider version
-      version = ">=0.13.0"
+      version = ">=0.14.0"
     }
   }
 }
