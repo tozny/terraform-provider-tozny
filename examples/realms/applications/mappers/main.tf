2ef094e125230974c24128d15f702c3c14ea6c20
Katie Rosas <katie@tozny.com>
2024-09-13 14:34:07 -0700

    Update version

--- examples/realms/applications/mappers/main.tf
+++ examples/realms/applications/mappers/main.tf
@@ -8,7 +8,7 @@ terraform {
       # for Tozny registry.terraform.io/tozny
       source = "tozny/tozny"
       # Pin Tozny provider version
-      version = ">=0.26.0"
+      version = ">=0.27.0"
     }
   }
 }
